4cf9bdf860441bb5452deef92313ecfe7ac8f268
TEH SQUAD CYBER 404 <52650869+teh-squadcyber@users.noreply.github.com>
2019-07-09 01:47:52 +0700

    Create tehscript.py

---
+++ tehscript.py
@@ -0,0 +1,137 @@
+#! usr/bin/python
+#TEH SQUAD CYBER {CoDay#XploiT}
+
+import os
+import sys
+
+def teh():
+    os.system ('clear')
+    teh = """\033[1;31;40m
+             /`\                                                       
+            / : |                                                      
+   _.._     | '/                                                       
+ /`    \    | /    {SCRIPT DEFACE TEH v1}
+|  .-._ '-"` (       
+|_/   /   o  o\       - TEH SQUAD CYBER 404 -               
+      |  =  () )=                                                      
+       \  '--`/                                                        
+       / ---<`                                                         
+      | ,    \\    INSTAGRAM :tehsquadcyber.id                                                    
+      | |     \\__                                                     
+      / ;     |.__)                                                    
+     (_/.-.   ;                                                        
+    { `|   \_/                                                        
+     '-\   / |                     
+        | /  |                  
+       /  \  '-.                                                       
+       \__|----'                                                       """
+
+
+    teh2 = """\033[1;32;40m
+             /`\
+            / : |
+   _.._     | '/
+ /`    \    | /    {SCRIPT DEFACE TEH v1}
+|  .-._ '-"` (
+|_/   /   o  o\       - TEH SQUAD CYBER 404 -
+      |  =  () )=
+       \  '--`/    copyright (c) Syntax7 - TSC2019
+       / ---<`
+      | ,    \\    INSTAGRAM :tehsquadcyber.id                             >
+      | |     \\__ GITHUB    :https://github.com/TEHSquadCyber
+      / ;     |.__)
+     (_/.-.   ;
+    { `|   \_/
+     '-\   / |   [!] SUKSES MEMBUAT >
+        | /  |   [!] FILE NAME TEH404.html >
+       /  \  '-.
+       \__|----'"""
+                         
+    print teh
+    tehtit = raw_input ("\033[1;31;40m------[$]TITLE : ")
+    tehnik = raw_input ("\033[1;37;40m------[$]NICK  : ")
+    tehtim = raw_input ("\033[1;31;40m------[$]TEAM  : ")
+    tehmes = raw_input ("\033[1;37;40m------[$]PESAN : ")
+    tehkon = raw_input ("\033[1;31;40m------[$]EMAIL : ")
+    tehgrt = raw_input ("\033[1;37;40m------[$]GREET : ")
+    squad = open ("TEH404.html","w")
+    create1 = """<html>
+<head>
+<link rel="SHORTCUT ICON" href="http://dev-xmen.pantheonsite.io/wp-content/uploads/2017/08/2q3abk0.jpg" type="image/x-icon"/>
+<meta content='Hacked By"""
+    create2 = tehnik
+    create3 = """' name='description'/><meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJ17yapGqo79czdxUmWbokgbW6Psu8dMx3WW4oTT0wPWcq_g7L" />
+
+  <link href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png" rel="shortcut icon" />
+<body bgcolor="black"><title>"""
+    create4 = tehtit
+    create5 = """</title>
+
+</style>
+</head>
+
+
+<div style="height: auto; min-height: 100%;">
+
+<div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 30%; left: 50%;">
+
+
+<img src="https://4.top4top.net/p_1272yj2di0.gif">
+<body>
+<div align="center">
+</div>
+<div align="center">
+ <pre style="font: 50px/10px courier;"><b><br><br> <br><br><br><br><br>
+<font color="white"> Hacked By <font color="red">"""
+    create6 = tehnik
+    create7 = """ </font> 
+</pre>
+<pre style="font: 30px/10px courier;"><b>"""
+    create8 = tehtim
+    create9 = """</b></pre>
+
+</div>
+<div align="center">
+<pre style="font: 20px/30px courier;"><b>"""
+    createa = tehmes
+    createb = """<i><div>
+</b></pre>
+</div>
+<div align="center">
+<pre>
+	
+<div style=?text-align:left;?>
+~root@ Greetz : """
+    createc = tehgrt
+    created = """</div>
+</pre>
+	<br> <b><font color="red" face="Ubuntu Mono" size="3"><i>Contact?<br> <font face="Ubuntu Mono" size="3" color="white"><i> """
+    createe = tehkon
+    createf = """<i> </font> </center><i><br>
+<br> <b><font color="red" face="Ubuntu Mono" size="3"><i>INDONESIAN <font face="Ubuntu Mono" size="3" color="white"><i>HACKER RULEZ<i> </font> </center><i>
+</pre>
+</div>
+</body>
+</html>
+  
+<iframe width="0" height="0" src="https://2.top4top.net/m_1272o1x4o0.mp3" frameborder="0" allowfullscreen</iframe>"""
+    squad.write(create1)
+    squad.write(create2)
+    squad.write(create3)
+    squad.write(create4)
+    squad.write(create5)
+    squad.write(create6)
+    squad.write(create7)
+    squad.write(create8)
+    squad.write(create9)
+    squad.write(createa)
+    squad.write(createb)
+    squad.write(createc)
+    squad.write(created)
+    squad.write(createe)
+    squad.write(createf)
+    squad.close()
+    os.system ('clear')
+    os.system ('sleep 3')
+    print teh2
+teh()
